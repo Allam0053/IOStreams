bf4816645365a983a7a9692662e6426fd2335971
Allam Taju Sarof <55141556+Allam0053@users.noreply.github.com>
2022-02-23 11:54:19 +0700

    io from socket

--- src/com/iostreams/app/Main.kt
+++ src/com/iostreams/app/Main.kt
@@ -1,12 +1,49 @@
 package com.iostreams.app
 
 import java.io.BufferedInputStream
+import java.io.BufferedOutputStream
 import java.io.DataInputStream
 import java.io.FileInputStream
+import java.net.Socket
 
 fun main (args: Array<String>) {
-    buffereInputStreamTry()
-    dataInputStreamTry()
+//    buffereInputStreamTry()
+//    dataInputStreamTry()
+    socketTry()
+}
+
+fun socketTry() {
+    try {
+        // 1. create a new socket object
+        var socket:Socket = Socket("monta.if.its.ac.id", 80)
+
+        // 2. obtain the input and output
+        var bis = BufferedInputStream( socket.getInputStream() )
+        var bos = BufferedOutputStream( socket.getOutputStream() )
+
+        // 3. exchange messages
+        bos.write( "Hello world\r\n\r\n".toByteArray() )
+        bos.flush()
+
+        //
+        var bResp = bis.readAllBytes()
+//        var bResp = bis.readNBytes( 100 )
+        var resp = String( bResp )
+        print( resp )
+
+//        var resp = ""
+//        while( bResp.size < 100 ) {
+//            resp += bResp.toString()
+//            bResp = bis.readNBytes( 100 )
+//        }
+
+//        print( resp )
+
+        // 4. close connection
+        socket.close()
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 }
 
 fun dataInputStreamTry () {
